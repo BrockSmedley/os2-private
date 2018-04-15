fca51d52e1b6d08bac22d9f9e3ef51a4b5b8cd94
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2018-04-15 03:15:57 -0700

    rdrand implementation

--- 1_concurrency/prog.c
+++ 1_concurrency/prog.c
@@ -4,6 +4,34 @@
 #include <sys/time.h>
 #include "./mt/mt19937ar.h"
 
+int randomseed(){
+  struct timeval tv;
+
+  gettimeofday(&tv, NULL);
+
+  unsigned long long millisecondsSinceEpoch =
+        (unsigned long long)(tv.tv_sec) * 1000 +
+	(unsigned long long)(tv.tv_usec) / 1000;
+  /* thanks https://stackoverflow.com/a/10889489 */
+
+  return millisecondsSinceEpoch;
+}
+
+int randomint_mt(int low, int high){
+  init_genrand(randomseed());
+  return (abs(genrand_int32()) % (high+1 - low) + low);
+}
+
+unsigned int randomint_rd(int low, int high){
+  unsigned int rnum;
+  __asm__ ("rdrand %[rnum]" 
+	: [rnum] "=r" (rnum)
+	: 
+	: "cc");
+  /* thanks https://codereview.stackexchange.com/a/150230 */
+  return (rnum % (high+1 - low) + low);
+}
+
 int main(int argc, char **argv)
 {
 
@@ -27,29 +55,16 @@ int main(int argc, char **argv)
 										
 	if(ecx & 0x40000000){
 		//use rdrand
-		printf("You gotta use rdrand!\n");
+		printf("Using rdrand!\n");
+		printf("%d\n", randomint_rd(1,10));
 	}
 	else{
 		//use mt19937
-  		printf("random: %d\n", randomint_mt(3,7));
+		printf("Using Mersenne Twister!\n");
+  		printf("%d\n", randomint_mt(3,7));
 	}
 	
 	return 0;
 }
 
-int randomseed(){
-  struct timeval tv;
 
-  gettimeofday(&tv, NULL);
-
-  unsigned long long millisecondsSinceEpoch =
-        (unsigned long long)(tv.tv_sec) * 1000 +
-	(unsigned long long)(tv.tv_usec) / 1000;
-
-  return millisecondsSinceEpoch;
-}
-
-int randomint_mt(int low, int high){
-  init_genrand(randomseed());
-  return (abs(genrand_int32()) % (high+1 - low) + low);
-}
