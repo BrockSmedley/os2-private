64fde5d66748e2f01e2189e5e09188859e395a68
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2018-04-15 16:45:45 -0700

    latest implementation

--- 1_concurrency/prog.c
+++ 1_concurrency/prog.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pthread.h>
+
 #include "./mt/mt19937ar.h"
+#include "./stack.h"
 
 #define BUFFSIZE 32
-#define rfunc (*rando)
+//#define randf (*rando)
 
-struct num {
-  int val;
-  int time;
-};
+unsigned int (* randf)(int,int);
+
+pthread_cond_t *notFull, *notEmpty;
+pthread_mutex_t *mootex;
 
 int randomseed(){
   struct timeval tv;
@@ -42,20 +48,33 @@ unsigned int randomint_rd(int low, int high){
 
 // dumps random numbers into a buffer
 // waits 3-7 seconds between productions
-void run_producer(int buffer[BUFFSIZE]){
-  // if buffer full
-  //   block until buffer opens up
-  // write data to buffer
-  return;
+void *run_producer(void *buff){
+  printf("Producer created!"); 
+
+  do {
+    printf("producer running\n");
+    
+    struct num numb;
+    numb.val = (*randf)(1,100);
+    numb.time = (*randf)(2,9);
+    push(buff,numb);
+    sleep((*randf)(3,7));
+  } while (1 == 1);
+  return (NULL);
 }
 
 // prints numbers dumped into buffer by producer
 // waits 2-9 seconds between consumptions
-void run_consumer(){
-  // if buffer empty
-  //   block until buffer gets some data
-  // consume data from buffer (print it)
-  return;
+void *run_consumer(void *buff){
+  printf("Consumer created!");
+  //pthread_join(pthread_self(), NULL);
+    printf("consumer running\n");
+  while (!isEmpty(buff)){
+      struct num n = pop(buff);
+      printf("%d\n", n.val);
+      sleep(n.time);
+  }
+  return (NULL);
 }
 
 
@@ -73,7 +92,15 @@ int main(int argc, char **argv)
 	unsigned int h = 7;							
 	
 	unsigned int (*rando)(int, int);
+	struct Stack *buffer = createStack(BUFFSIZE);
 
+	/* thanks https://docs.oracle.com/cd/E19253-01/816-5137/tlib-95520/index.html */
+	pthread_t tidc;
+	pthread_t tidp;
+	void *arg;
+	int ret;
+	int *status;
+	
 	eax = 0x01;
 	
 	__asm__ __volatile__(
@@ -86,17 +113,29 @@ int main(int argc, char **argv)
 		//use rdrand
 		printf("Using rdrand!\n");
 		//printf("%d\n", randomint_rd(1,10));
-		rando = &randomint_rd;
+		randf = &randomint_rd;
 	}
 	else{
 		//use mt19937
 		printf("Using Mersenne Twister!\n");
   		//printf("%d\n", randomint_mt(3,7));
-		rando = &randomint_mt;
+		randf = &randomint_mt;
 	}
 
-	printf("%d\n", rfunc(3,7));
+	//printf("%d\n", (* randf)(3,7));
 	
+	// create a producer
+	pthread_create(&tidp, NULL, run_producer, buffer);
+	pthread_detach(tidp);
+
+	// create a consumer
+	pthread_create(&tidc, NULL, run_consumer, buffer);
+	pthread_detach(tidc);
+	
+
+	//exit(0);
+	// suicide
+	// ...
 	return 0;
 }
 
